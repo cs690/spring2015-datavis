d088849441b1ce3eeb39db5cc44e3b390ad3a99f
Azure22 <hanx.azure22@gmail.com>
2015-03-03 00:34:23 -0800

    Combine into vis.js

--- xihan/week05/sketch.js
+++ xihan/week05/sketch.js
@@ -2,25 +2,23 @@ var svg1 = d3.select("#chart1").append("svg");
 var svg2 = d3.select("#chart2").append("svg");
 
 svg1.append("circle")
-	.attr("id", "abc")
+	.attr("class", "bar abc")
 	.attr("r", 3.5)
 	.attr("cx", 50)
 	.attr("cy", 50)
 	.style("fill", function(d) { return "rgb(255, 0, 0)"; })
 	.style({opacity:'0.2',})
 	.on('mouseover', function(d){
-		svg1.select("#abc").style({opacity:'1.0',});
-		svg2.select("#abc").style({opacity:'1.0',});
+		d3.selectAll(".abc").style({opacity:'1.0',});
 	});
 	
 svg2.append("circle")
-	.attr("id", "abc")
+	.attr("class", "scattor abc")
 	.attr("r", 3.5)
 	.attr("cx", 50)
 	.attr("cy", 50)
 	.style("fill", function(d) { return "rgb(0, 255, 0)"; })
 	.style({opacity:'0.2',})
 	.on('mouseover', function(d){
-		svg1.select("#abc").style({opacity:'1.0',});
-		svg2.select("#abc").style({opacity:'1.0',});
+		d3.selectAll(".abc").style({opacity:'1.0',});
 	});
